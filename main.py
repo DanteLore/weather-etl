62fe21f357cd4570b0379ded56d8e30f8c7de37d
Dan Taylor <dan.taylor@thetrainline.com>
2021-12-13 12:48:05 +0000

    Pass data between ETL steps as a file, handle errors, implement DLQ

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from weather_etl import load_observations_data_to_local
+from weather_etl import extract_observations_data, transform_observations_data
 
 # https://www.metoffice.gov.uk/services/data/datapoint
 
+INPUT_FILE = 'weatherData/response.json'
+OUTPUT_FILE = 'weatherData/last_24h_observations_uk.json'
 
 if __name__ == '__main__':
-    load_observations_data_to_local('weatherData/last_24h_observations_uk.json')
+    extract_observations_data(INPUT_FILE)
+    transform_observations_data(INPUT_FILE, OUTPUT_FILE)
--- weather_etl.py
+++ weather_etl.py
@@ -10,12 +10,6 @@ BASE_PARAMS = {
     "res": "hourly"
 }
 
-TEMP_FILE = "/tmp/observations.json"
-S3_BUCKET = "dantelore.data.incoming"
-S3_FILENAME = "weather/{year}/{month}/{day}/observations.json".format(year=datetime.now().year,
-                                                                      month=datetime.now().month,
-                                                                      day=datetime.now().day)
-
 
 def get_sites():
     url = BASE_URL + "val/wxobs/all/json/sitelist"
@@ -39,19 +33,22 @@ def print_observations_fields():
             print("{name}: {$} ({units})".format(**p))
 
 
-def extract_observations_data():
+def extract_observations_data(filename):
     url = BASE_URL + "val/wxobs/all/json/all"
     response = requests.get(url, params=BASE_PARAMS)
 
     print("Received status {0} from {1}".format(response.status_code, url))
 
     if response.status_code == 200:
-        return response.json()
+        with open(filename, 'w') as f:
+            json.dump(response.json(), f, ensure_ascii=False, indent=4)
+
+        return True
     else:
-        return None
+        return False
 
 
-def transform_observations_data(data):
+def do_transform(data):
     params = data['SiteRep']['Wx']['Param']
     names_lookup = dict([(p['name'], p['$'].lower().replace(' ', '_')) for p in params])
     obs = data['SiteRep']['DV']['Location']
@@ -76,48 +73,37 @@ def transform_observations_data(data):
             day = datetime.strptime(p['value'], "%Y-%m-%dZ")
 
             for r in p['Rep']:
-                try:
-                    time = day + timedelta(minutes=int(r['$']))
-                    row = {
-                        "timestamp": time.isoformat(),
-                        "site_id": site_id,
-                        "site_name": site_name,
-                        "site_country": site_country,
-                        "site_continent": site_continent,
-                        "site_elevation": site_elevation,
-                        "lat": lat,
-                        "lon": lon
-                    }
+                time = day + timedelta(minutes=int(r['$']))
+                row = {
+                    "timestamp": time.isoformat(),
+                    "site_id": site_id,
+                    "site_name": site_name,
+                    "site_country": site_country,
+                    "site_continent": site_continent,
+                    "site_elevation": site_elevation,
+                    "lat": lat,
+                    "lon": lon
+                }
 
-                    observations = dict((names_lookup[key], r[key]) for key in r if key != '$')
-                    row.update(observations)
-                    yield json.dumps(row)
-                except Exception as e:
-                    print("Parsing data row in 'Rep' failed")
-                    print(e)
+                observations = dict((names_lookup[key], r[key]) for key in r if key != '$')
+                row.update(observations)
+                yield json.dumps(row)
 
 
-def load_observations_data_to_local(filename):
-    data = extract_observations_data()
+def transform_observations_data(input_filename, output_filename):
+    with open(input_filename, 'r') as f:
+        data = json.load(f)
 
     if not data:
         return False
 
-    with open(filename, 'w') as f:
+    with open(output_filename, 'w') as f:
         i = 0
-        for line in transform_observations_data(data):
+        for line in do_transform(data):
             i += 1
             f.writelines(line + '\n')
 
-        print("Wrote {0} lines to file '{1}'".format(i, filename))
+        print("Wrote {0} lines to file '{1}'".format(i, output_filename))
 
     return True
 
-
-def load_observations_data_to_s3():
-    if load_observations_data_to_local(TEMP_FILE):
-        s3 = boto3.resource('s3')
-        s3.Bucket(S3_BUCKET).upload_file(
-            TEMP_FILE,
-            S3_FILENAME
-        )
