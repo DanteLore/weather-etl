56833cafa1c4c22f05a7d1a540e14af079f8a34c
Dan Taylor <dan.taylor@thetrainline.com>
2022-01-04 13:02:40 +0000

    Added a simple backfill script

--- aws_helpers.py
+++ aws_helpers.py
@@ -12,6 +12,16 @@ def load_file_to_s3(filename, s3_bucket, s3_key):
     )
 
 
+def download_file_from_s3(s3_bucket, s3_key, filename):
+    print("Downloading data from S3://{0}/{1}".format(s3_bucket, s3_key))
+
+    s3 = boto3.resource('s3')
+    s3.Bucket(s3_bucket).download_file(
+        s3_key,
+        filename
+    )
+
+
 def add_glue_partition_for(year, month, day):
     sql = f"ALTER TABLE weather ADD IF NOT EXISTS PARTITION (year='{year}', month='{month}', day='{day}')"
     execute_athena_command(sql)
